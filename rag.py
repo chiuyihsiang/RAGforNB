0ec814b056e701c28e9d309063cc8870946bea6b
bevis <jimmy930226@gmail.com>
2025-09-16 18:22:01 +0800

    load the yaml

--- rag.py
+++ rag.py
@@ -1,1 +1,10 @@
-print("hello")
+import sentence_transformers, yaml, transformers, chromadb
+
+#open yaml files
+files = ["SO-XP.yaml", "SO-11P.yaml"]
+data = {}
+for f in files:
+    with open(f, "r", encoding = "utf-8") as file:
+        data[f] = yaml.safe_load(file)
+
+print(data["SO-XP.yaml"])
