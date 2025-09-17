6bb3a77b7b045a50f6a1818f4f0b3a7a1a11d3f6
bevis <jimmy930226@gmail.com>
2025-09-17 12:45:36 +0800

    load embeding model

--- rag.py
+++ rag.py
@@ -1,4 +1,4 @@
-import sentence_transformers, yaml, transformers, chromadb
+import yaml, chromadb, sentence_transformers, transformers, langchain
 
 #open yaml files
 files = ["SO-XP.yaml", "SO-11P.yaml"]
@@ -7,4 +7,8 @@ for f in files:
     with open(f, "r", encoding = "utf-8") as file:
         data[f] = yaml.safe_load(file)
 
-print(data["SO-XP.yaml"])
+#embedding model loading
+embedding_model = sentence_transformers.SentenceTransformer("BAAI/bge-m3")
+
+# Initialize ChromaDB
+client = chromadb.Client()
